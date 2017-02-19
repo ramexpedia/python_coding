23aea1d46614b6f62a851a7785659dc866c7b61a
Janakiram Pulipati <ramexpedia@users.noreply.github.com>
2017-02-19 10:37:11 -0800

    Add files via upload

---
+++ facebook-data.py
@@ -0,0 +1,27 @@
+import facebook
+import requests
+from sys import argv
+import pandas as pr
+
+script, filename = argv
+
+access_token = '1850864201834154|xKFUOCe-FWiSH_9lccM7xJMVbc0'
+user = 'expedia'
+graph = facebook.GraphAPI(access_token)
+profile = graph.get_object(user)
+posts = graph.get_connections(profile['id'], 'posts')
+post_data = posts['data']
+
+print("Data gathered from FB API")
+
+date = [str(post_data[i]['created_time'].encode('ascii', 'ignore').decode('ascii', 'ignore'))for i in range(0,8)]
+
+message = [str(post_data[i]['message'].encode('ascii', 'ignore').decode('ascii', 'ignore')) for i in range(0,8)]
+
+pr2 = pr.DataFrame({'Date':date,'Message':message})
+
+print("Writing data to file at " + filename )
+pr2.to_csv(filename, index= False, sep='\t', mode='a')
+
+print("Program completed successfully!")
+print("If you're a mac/linux user please run this command to see output - 'open " + filename + "'")
